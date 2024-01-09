fb3fd0b7e59ecc2da380cf92f72408c7345ef83a
Rene <racosta323@gmail.com>
2024-01-09 06:55:18 -0600

    lab complete

--- index.js
+++ index.js
@@ -1,2 +1,24 @@
+let num1;
+let num2;
+let multiply = num1*num2;
 
-// Write your code here
+num1 = multiply / num2;
+num2 = multiply / num1;
+
+multiply = 62;
+
+let random = Math.floor(Math.random()*10);
+
+// let mod = 211/12; these numbers equal 4 when divided
+let num3;
+let num4;
+let num5;
+let mod = num3 - num4*num5;
+
+num3=mod+num4*num5;
+num4=(num3-mod)/num5;
+
+mod=4;
+
+let max = Math.max(1, 2, 3, 4, 20);
+
